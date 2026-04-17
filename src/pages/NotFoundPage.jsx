53613fb4a38a43321dabbd9df32a0f598aca6fb8
MatosChuquinoJesusPresencio <elcevichexd06>
2026-04-17 16:32:36 -0500

    Realizacion de la pagina de 404

--- src/pages/NotFoundPage.jsx
+++ src/pages/NotFoundPage.jsx
@@ -1,8 +1,30 @@
+import { Link } from "react-router-dom";
+
 const NotFoundPage = () => {
     return (
-        <>
-        <h1>PAGINA DE NOT FOUND</h1>
-        </>
+        <div className="container-fluid vh-100 d-flex justify-content-center align-items-center bg-main text-light">
+            
+            <div className="text-center">
+                
+                <h1 className="display-1 fw-bold">
+                    404
+                </h1>
+
+                <h2 className="mb-3">
+                    Página no encontrada
+                </h2>
+
+                <p className="mb-4">
+                    La página que estás buscando no existe o fue movida.
+                </p>
+
+                <Link to="/" className="btn btn-warning fw-bold">
+                    Volver al inicio
+                </Link>
+
+            </div>
+
+        </div>
     );
 };
 
