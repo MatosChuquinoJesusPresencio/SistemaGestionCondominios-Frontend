b39dbaa306b7bb18a399313d42400bf1102db902
Juan Victor <figyuptonj99@gmail.com>
2026-04-30 20:33:01 -0500

    feat: agregar estilos de Bootstrap, Slick Carousel y AOS en App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,14 @@
 import AppRouter from "./routers/AppRouter";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.min.js';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+// Slick Carousel (Estilos del slider)
+import "slick-carousel/slick/slick.css"; 
+import "slick-carousel/slick/slick-theme.css";
+
+// Animaciones AOS
+import 'aos/dist/aos.css';
 
 const App = () => {
   return <AppRouter />;
